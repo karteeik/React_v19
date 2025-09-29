b2c79d265531895d5bff6529d6aaf82ee35c495f
kartikmangukiya <kmtech012@gmail.com>
2025-09-29 15:20:55 +0530

    complete crud

--- FullStackReact/src/Api/GetData.jsx
+++ FullStackReact/src/Api/GetData.jsx
@@ -3,14 +3,22 @@ const api = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com/",
 });
 
+// read
 export const getCrudData = () => {
   return api.get("posts");
 };
 
+// delete
 export const deletePost = (id) => {
-  return api.delete(`posts/${id}`)
-}
+  return api.delete(`posts/${id}`);
+};
 
+// create
 export const postsData = (post) => {
   return api.post("/posts", post);
-}
+};
+
+// update
+// export const updateData = (id, post) => {
+//   return api.put(`/posts/${id}`, post);
+// };
